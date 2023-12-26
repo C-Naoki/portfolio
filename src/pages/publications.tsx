0ad35a5fae51329207b7cc2c4c683e0dd028d15c
naoki <u429561d@ecs.osaka-u.ac.jp>
2023-12-26 12:17:10 +0900

    :memo: add: new documents
    AstronComput45, DEIM2023

--- src/pages/publications.tsx
+++ src/pages/publications.tsx
@@ -14,9 +14,9 @@ export default function Publications() {
           周期解析による変動天体の検出<br />
           第15回データ工学と情報マネジメントに関するフォーラム (DEIM2023), 4a-6-3, 2023.<br />
           <span style={{ fontWeight: 'bold', textDecoration: 'underline' }}>千原 直己</span>, 高田 唯史, 藤原 靖宏, 鬼塚 真<br />
-          <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-29cb4.appspot.com/o/deim2023%2Fpaper.pdf?alt=media&token=494477cf-dcc0-4aac-b546-ee25553dab8f" target="_blank" rel="noopener noreferrer" className={styles.link}>[paper]</a>{' '}
-          <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-29cb4.appspot.com/o/deim2023%2Fslides.pdf?alt=media&token=45e0e3f8-d602-43b8-b490-32c3fa60aad4" target="_blank" rel="noopener noreferrer" className={styles.link}>[slides]</a>{' '}
-          <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-29cb4.appspot.com/o/deim2023%2Fposter.pdf?alt=media&token=29a0c9be-120e-4faa-94bd-c974fe0dc8c8" target="_blank" rel="noopener noreferrer" className={styles.link}>[poster]</a>
+          <a href="/assets/DEIM2023/paper.pdf" target="_blank" rel="noopener noreferrer" className={styles.link}>[paper]</a>{' '}
+          <a href="/assets/DEIM2023/slides.pdf" target="_blank" rel="noopener noreferrer" className={styles.link}>[slides]</a>{' '}
+          <a href="/assets/DEIM2023/poster.pdf" target="_blank" rel="noopener noreferrer" className={styles.link}>[poster]</a>
         </li>
       </ul>
 
@@ -26,7 +26,7 @@ export default function Publications() {
           Effective detection of variable celestial objects using machine learning-based periodic analysis<br />
           Astronomy and Computing, Vol. 45, pp. 100765, 3 November 2023.<br />
           <span style={{ fontWeight: 'bold', textDecoration: 'underline' }}>Naoki Chihara</span>, Tadafumi Takata, Yasuhiro Fujiwara, Koki Noda, Keisuke Toyoda, Kaito Higuchi, Makoto Onizuka<br />
-          <a href="https://firebasestorage.googleapis.com/v0/b/portfolio-29cb4.appspot.com/o/astronomy%20and%20computing2023%2Fpaper.pdf?alt=media&token=e6539daa-8030-47b5-b134-e5e20d77ad3d" target="_blank" rel="noopener noreferrer" className={styles.link}>[paper]</a>
+          <a href="/assets/AstronComput45/paper.pdf" target="_blank" rel="noopener noreferrer" className={styles.link}>[paper]</a>
         </li>
       </ul>
 
