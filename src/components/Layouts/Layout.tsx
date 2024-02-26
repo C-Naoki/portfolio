7e474427597f95843bc747c77703bbff9cdb5aa5
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-02-26 23:06:21 +0900

    :art: fix: change folder's name

--- src/components/Layouts/Layout.tsx
+++ src/components/Layouts/Layout.tsx
@@ -1,11 +1,11 @@
-import React from 'react';
 import { Container } from '@material-ui/core';
+import React from 'react';
 import { Toaster } from 'react-hot-toast';
 
 
 export default function Layout({ children, title }: { children: React.ReactNode, title?: string }) {
   return (
-    <Container maxWidth="md" style={{ marginTop: '100px' }}>
+    <Container style={{ marginTop: '95px', maxWidth: '750px', backgroundColor: '#ffffff', paddingTop: '5px'  }}>
       {title && <h1>{title}</h1>}
       {children}
       <Toaster />
