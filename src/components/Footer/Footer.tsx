ac65dc55c0f59809f96335ebd2cee640a51e7cc1
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-03-03 00:47:20 +0900

    :sparkles: feat: automatically display last updated date

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,29 @@
+import React, { useEffect, useState } from 'react';
+
 export const Footer = () => {
+  const [lastUpdated, setLastUpdated] = useState('');
+
+  useEffect(() => {
+    const fetchLastCommitDate = async () => {
+      const response = await fetch('https://api.github.com/repos/C-Naoki/portfolio/commits?per_page=1');
+      const commits = await response.json();
+      if (commits.length > 0) {
+        const lastCommitDate = new Date(commits[0].commit.committer.date);
+        const formattedDate = lastCommitDate.toLocaleDateString('en-US', {
+          year: 'numeric',
+          month: 'short',
+          day: 'numeric',
+        });
+        setLastUpdated(formattedDate);
+      }
+    };
+
+    fetchLastCommitDate();
+  }, []);
+
   return (
     <footer style={{ justifyContent: 'space-between', alignItems: 'center' }}>
-      <p>© 2023 Naoki Chihara. All rights reserved. Last updated on Feb. 13, 2024</p>
+      <p>© 2023 Naoki Chihara. All rights reserved. Last updated on {lastUpdated}</p>
     </footer>
   );
 };
