4861385281bcb7dc2305e95acc2455fe00d54dd4
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-03-12 15:52:48 +0900

    :sparkles: feat: Improve responsiveness of header on mobile devices
    display botton for switching theme (light/dark)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,4 @@
-import { Drawer, IconButton, makeStyles } from '@material-ui/core';
+import { Drawer, IconButton } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import Button from '@material-ui/core/Button';
 import Toolbar from '@material-ui/core/Toolbar';
@@ -7,6 +7,7 @@ import { Menu as MenuIcon } from "@material-ui/icons";
 import Link from 'next/link';
 import { useState } from 'react';
 import { FaGithub, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import styles from '../../styles/header.module.css';
 import DrawerContent from './DrawerContent';
 import LanguageSwitcher from './LanguageSwitcher';
 import ThemeSwitcher from './ThemeSwitcher';
@@ -16,80 +17,10 @@ type AnimatedButtonProps = {
   children: React.ReactNode;
 };
 
-const useStyles = makeStyles((theme) => ({
-  appBar: {
-    background: 'var(--header-color)',
-  },
-  menuButton: {
-    position: 'absolute',
-    right: theme.spacing(2),
-    [theme.breakpoints.up('md')]: {
-      display: 'none',
-    },
-  },
-  headerOptions: {
-    flexGrow: 1,
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'space-evenly',
-    [theme.breakpoints.down('sm')]: {
-      display: 'none',
-    },
-  },
-  toolbarCenter: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    flexGrow: 1,
-    display: 'flex',
-    alignItems: 'center',
-    fontSize: '1.3rem',
-  },
-  iconSpacing: {
-    marginLeft: theme.spacing(1),
-    marginRight: theme.spacing(1.5),
-    fontSize: '1.1rem',
-  },
-  LanguageSwitcher: {
-    marginLeft: '40px',
-    marginRight: '-40px',
-  },
-  ThemeSwitcher: {
-    marginLeft: '35px',
-    marginRight: '-40px',
-  },
-  buttonLine: {
-    position: 'relative',
-    overflow: 'hidden',
-    '&:hover': {
-      backgroundColor: 'transparent',
-    },
-    '&::before': {
-      content: '""',
-      position: 'absolute',
-      width: '100%',
-      height: '1px',
-      bottom: '0',
-      left: '0',
-      backgroundColor: '#d4d4d4',
-      transform: 'scaleX(0)',
-      transition: 'transform 0.3s ease-in-out',
-      transformOrigin: 'center top',
-    },
-    '&:hover::before': {
-      transform: 'scaleX(1)',
-      transformOrigin: 'center top',
-    },
-  },
-}));
-
 const AnimatedButton: React.FC<AnimatedButtonProps> = ({ href, children }) => {
-  const classes = useStyles();
   return (
     <Link href={href} passHref>
-      <Button className={classes.buttonLine} color="inherit">
+      <Button className={styles.buttonLine} color="inherit">
         {children}
       </Button>
     </Link>
@@ -98,7 +29,6 @@ const AnimatedButton: React.FC<AnimatedButtonProps> = ({ href, children }) => {
 
 const Header = () => {
   const [drawerOpen, setDrawerOpen] = useState(false);
-  const classes = useStyles();
 
   const handleDrawerToggle = () => {
     setDrawerOpen(!drawerOpen);
@@ -106,40 +36,43 @@ const Header = () => {
 
   return (
     <>
-      <AppBar position="fixed" className={classes.appBar}>
-        <Toolbar className={classes.toolbarCenter}>
-          <Typography variant="h6" className={classes.title}>
+      <AppBar position="fixed" className={styles.appBar}>
+        <Toolbar className={styles.toolbarCenter}>
+          <Typography variant="h6" className={styles.title}>
             Naoki Chihara
           </Typography>
-          <div className={classes.headerOptions}>
+          <div className={styles.headerOptions}>
             <AnimatedButton href="/">Home</AnimatedButton>
             <AnimatedButton href="/blog-under-construction">Blog</AnimatedButton>
             <AnimatedButton href="/publications">Publications</AnimatedButton>
             <AnimatedButton href="/contact">Contact</AnimatedButton>
-            <div className={classes.toolbarCenter}>
-              <a href="https://github.com/C-Naoki" target="_blank" rel="noopener noreferrer" className={classes.toolbarCenter}>
-                <FaGithub className={classes.iconSpacing} />
+            <div className={styles.toolbarCenter}>
+              <a href="https://github.com/C-Naoki" target="_blank" rel="noopener noreferrer" className={styles.toolbarCenter}>
+                <FaGithub className={styles.iconSpacing} />
               </a>
-              <a href="https://twitter.com/C__Naoki" target="_blank" rel="noopener noreferrer" className={classes.toolbarCenter}>
-                <FaTwitter className={classes.iconSpacing} />
+              <a href="https://twitter.com/C__Naoki" target="_blank" rel="noopener noreferrer" className={styles.toolbarCenter}>
+                <FaTwitter className={styles.iconSpacing} />
               </a>
-              <a href="https://www.linkedin.com/in/naoki-chihara-0a35a827a/" target="_blank" rel="noopener noreferrer" className={classes.toolbarCenter}>
-                <FaLinkedin className={classes.iconSpacing} />
+              <a href="https://www.linkedin.com/in/naoki-chihara-0a35a827a/" target="_blank" rel="noopener noreferrer" className={styles.toolbarCenter}>
+                <FaLinkedin className={styles.iconSpacing} />
               </a>
-              <a className={classes.LanguageSwitcher}>
+              <a className={styles.LanguageSwitcher}>
                 <LanguageSwitcher />
               </a>
-              <a className={classes.ThemeSwitcher}>
+              <a className={styles.ThemeSwitcher}>
                 <ThemeSwitcher />
               </a>
             </div>
           </div>
+          <a className={styles.ThemeSwitcherSmall}>
+            <ThemeSwitcher />
+          </a>
           <IconButton
             edge="end"
             color="inherit"
             aria-label="menu"
             onClick={handleDrawerToggle}
-            className={classes.menuButton}
+            className={styles.menuButton}
           >
             <MenuIcon />
           </IconButton>
