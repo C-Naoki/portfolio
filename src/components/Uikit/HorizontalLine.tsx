cb5754420dbcef68b1f07d378b6a3cf19369b901
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-03-30 13:13:52 +0900

    💄 style: make holizontalline darker color when dark mode
    - in addition, copied style code to global.css

--- src/components/Uikit/HorizontalLine.tsx
+++ src/components/Uikit/HorizontalLine.tsx
@@ -1,21 +1,5 @@
-import type { CSSProperties } from 'react'
-import React from 'react'
-
-interface PropsType {
-  style?: CSSProperties
-}
-
-const HorizontalLine: React.FC<PropsType> = (props: PropsType) => {
-  const defaultStyle: CSSProperties = {
-    marginTop: '0px',
-    marginBottom: '10px',
-    width: '100%',
-    height: '1px',
-    backgroundColor: '#CFD8D8',
-    marginLeft: '0px'
-  }
-
-  return <div style={props.style ?? defaultStyle}></div>
+const HorizontalLine = (): JSX.Element => {
+  return <div className='horizontal-line'></div>
 }
 
 export default HorizontalLine
