337ef8d5496f13af5d8a0c8589649540233f05d8
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-04-03 19:30:33 +0900

    ⚡️ feat: Implement caching to enhance API response speed

--- src/pages/api/files.tsx
+++ src/pages/api/files.tsx
@@ -7,13 +7,28 @@ interface FileListResponse {
   files: string[]
 }
 
+const fileCache = new Map()
+
+async function getFileList (directoryPath: string): Promise<string[]> {
+  if (fileCache.has(directoryPath)) {
+    return fileCache.get(directoryPath)
+  }
+
+  const files = await readdir(directoryPath)
+  const pdfFiles = files.filter(file => file.endsWith('.pdf'))
+
+  fileCache.set(directoryPath, pdfFiles)
+  setTimeout(() => fileCache.delete(directoryPath), 1000 * 60 * 60 * 24)
+
+  return pdfFiles
+}
+
 export default async function handler (req: NextApiRequest, res: NextApiResponse<FileListResponse>): Promise<void> {
   const conferenceName = req.query.conferenceName as string
   const directoryPath = path.join(process.cwd(), 'public', 'assets', conferenceName)
 
   try {
-    const files = await readdir(directoryPath)
-    const pdfFiles = files.filter(file => file.endsWith('.pdf'))
+    const pdfFiles = await getFileList(directoryPath)
     res.status(200).json({ files: pdfFiles })
   } catch (err) {
     console.error(err)
