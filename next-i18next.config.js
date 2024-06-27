4d1383955d213f62319285090b7bbec06c98c955
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-06-27 12:38:49 +0900

    🔧 fix: specifies Japanese as fallback language

--- next-i18next.config.js
+++ next-i18next.config.js
@@ -1,11 +1,15 @@
 /** @type {import('next-i18next').UserConfig} */
 
-const path = require('path');
+const path = require('path')
 
 module.exports = {
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'ja'],
     localePath: path.resolve('./public/locales'),
-  },
+    fallbackLng: {
+      default: ['en'],
+      en: ['ja']
+    }
+  }
 }
