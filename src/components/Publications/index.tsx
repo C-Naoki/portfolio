14cc81196e5e9b78d939cd52e4a804af5ce9b96c
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-09-20 15:42:18 +0900

    🐛 fix: define types for deployment

--- src/components/Publications/index.tsx
+++ src/components/Publications/index.tsx
@@ -4,6 +4,7 @@ import AuthorList from './AuthorList'
 import Awards from './Awards'
 import Resources from './Resources'
 import Title from './Title'
+import type { VenueProps } from './Venue'
 import Venue from './Venue'
 
 import externalLinksInfo from '@/constants/externalLinksInfo'
@@ -12,14 +13,16 @@ export default function Publication ({ name }: { name: string }): JSX.Element {
   const { t } = useTranslation()
   const links = externalLinksInfo[name]
   const authors = t(`publications.${name}.authors`, { returnObjects: true }) as string[]
+  const venue = t(`publications.${name}.venue`, { returnObjects: true }) as VenueProps
+  const awards = t(`publications.${name}.awards`, { returnObjects: true }) as string[]
 
   return (
     <div className='publication'>
       <Title href={links.title} name={t(`publications.${name}.title`)} />
       <AuthorList authors={authors} />
-      <Venue {...t(`publications.${name}.venue`, { returnObjects: true })} />
+      <Venue name={venue.name} others={venue.others} />
       <Resources resources={links.resources} />
-      <Awards awards={t(`publications.${name}.awards`, { returnObjects: true })} urls={links.awards} />
+      <Awards awards={awards} urls={links.awards} />
     </div>
   )
 }
