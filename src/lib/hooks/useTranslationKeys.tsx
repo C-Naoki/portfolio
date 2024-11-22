5543f9f57786f503c3a2e3e092370b7c08633363
naoki <u429561d@ecs.osaka-u.ac.jp>
2024-11-23 02:46:45 +0900

    ✨ feat: add `useTranslationKeys` to get key lists from json file

---
+++ src/lib/hooks/useTranslationKeys.tsx
@@ -0,0 +1,10 @@
+import type { i18n } from 'i18next'
+
+const useTranslationKeys = (i18n: i18n, root: string): string[] => {
+  const translation = i18n.store.data[i18n.language].common as Record<string, string>
+  const keys = Object.keys(translation[root])
+
+  return keys
+}
+
+export default useTranslationKeys
