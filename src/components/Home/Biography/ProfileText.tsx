8ef11486df78e81a439aa9e0331c0699d50da629
naoki <u429561d@ecs.osaka-u.ac.jp>
2025-07-06 02:14:36 +0900

    ✨ feat: display Japanese CV

--- src/components/Home/Biography/ProfileText.tsx
+++ src/components/Home/Biography/ProfileText.tsx
@@ -1,3 +1,4 @@
+import { useRouter } from 'next/router'
 import { Trans } from 'next-i18next'
 import { FaDownload } from 'react-icons/fa'
 
@@ -5,21 +6,27 @@ import TransLink from '@/components/Uikit/TransLink'
 import externalLinksInfo from '@/constants/externalLinksInfo'
 
 const ProfileText = (): JSX.Element => {
+  const { locale } = useRouter()
   const components: Record<string, JSX.Element> = {}
 
   for (const [key, url] of Object.entries(externalLinksInfo.biography)) {
     components[key] = <TransLink url={url as string} />
   }
 
+  const cvUrl =
+    locale === 'ja'
+      ? externalLinksInfo.cv.replace('cv.pdf', 'cv_ja.pdf')
+      : externalLinksInfo.cv
+
   return (
-    <div className='profile-text'>
+    <div className="profile-text">
       <Trans
         i18nKey="profile-text"
         components={components}
       />
       <div style={{ marginTop: '15px' }}>
         <FaDownload className="fa-download"/>
-        <Trans i18nKey="cv" components={{ link_cv: <TransLink url={externalLinksInfo.cv}/> }}/>
+        <Trans i18nKey="cv" components={{ link_cv: <TransLink url={cvUrl} /> }} />
       </div>
     </div>
   )
